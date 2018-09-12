d7e5698cfcd9895aefd131b5504130be10ec6a6a
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-09-12 16:10:22 -0400

    Implement a better NullString type

---
+++ types/null/string.go
@@ -0,0 +1,183 @@
+package null
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+)
+
+// NullString is a wrapper around the database/sql NullString type that
+// implements all of the encoding/type interfaces that sql.NullString doesn't
+// implement out of the box.
+//
+// If the NullString is valid and contains the empty string, it will be
+// considered non-nil, and zero.
+type NullString struct {
+	sql.NullString
+}
+
+// String creates a new NullString based on the type and value of the given
+// interface. This function intentionally sacrafices compile-time safety for
+// developer convenience.
+//
+// If the interface is nil or a nil *string, the new NullString will be null.
+//
+// If the interface is a string or a non-nil *string, the new NullString will be
+// valid, and will be initialized with the (possibly dereferenced) value of the
+// interface.
+//
+// If the interface is any other type this function will panic.
+func String(i interface{}) NullString {
+	switch v := i.(type) {
+	case string:
+		return StringFrom(v)
+	case *string:
+		return StringFromPtr(v)
+	case nil:
+		return NullString{}
+	}
+	panic(fmt.Errorf(
+		"null.String: the given argument (%#v of type %T) was not of type "+
+			"string, *string, or nil", i, i))
+}
+
+// StringFrom creates a valid String from s.
+func StringFrom(s string) NullString {
+	return NullString{sql.NullString{
+		String: s,
+		Valid:  true,
+	}}
+}
+
+// StringFromPtr creates a valid String from *s.
+func StringFromPtr(s *string) NullString {
+	if s == nil {
+		return NullString{}
+	}
+	return StringFrom(*s)
+}
+
+// ValueOrZero returns the value of this NullString if it is valid; otherwise it
+// returns the zero value for a string.
+func (s NullString) ValueOrZero() string {
+	if !s.Valid {
+		return ""
+	}
+	return s.String
+}
+
+// Ptr returns a pointer to this NullString's value if it is valid; otherwise
+// returns a nil pointer. The captured pointer will be able to modify the value
+// of this NullString.
+func (s *NullString) Ptr() *string {
+	if !s.Valid {
+		return nil
+	}
+	return &s.String
+}
+
+// Set modifies the value stored in this NullString, and guarantees it is valid.
+func (s *NullString) Set(v string) {
+	s.String = v
+	s.Valid = true
+}
+
+// Null marks this NullString as null with no meaningful value.
+func (s *NullString) Null() {
+	s.Valid = false
+}
+
+// Interface
+
+// IsNil implements the pyrrho/encoding IsNiler interface. It will return true
+// if this NullString is null.
+func (s NullString) IsNil() bool {
+	return !s.Valid
+}
+
+// IsZero implements the pyrrho/encoding IsZeroer interface. It will return true
+// if this NullString is null or if its value is 0.
+func (s NullString) IsZero() bool {
+	return !s.Valid || s.String == ""
+}
+
+// MarshalText implements the encoding TextMarshaler interface. It will return
+// the value of this NullString if valid, or an empty string otherwise.
+func (s NullString) MarshalText() ([]byte, error) {
+	if !s.Valid {
+		return []byte{}, nil
+	}
+	return []byte(s.String), nil
+}
+
+// UnmarshalText implements the encoding TextUnmarshaler interface. It will
+// decode a given []byte into this NullString. Empty []bytes will result in a
+// valid-but-empty NullString, nil []bytes will result in a null NullString. All
+// other input -- including the string "null" -- will result in a valid
+// NullString.
+//
+// If the decode fails, the value of this NullString will be unchanged.
+func (s *NullString) UnmarshalText(text []byte) error {
+	if text == nil {
+		s.String = ""
+		s.Valid = false
+	}
+	s.String = string(text)
+	s.Valid = s.String != ""
+	return nil
+}
+
+// MarshalJSON implements the encoding/json Marshaler interface. It will return
+// the value of this NullString if valid, otherwise 'null'.
+func (s NullString) MarshalJSON() ([]byte, error) {
+	if !s.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(s.String)
+}
+
+// UnmarshalJSON implements the encoding/json Unmarshaler interface. It will
+// decode a given []byte into this NullString, so long as the provided []byte
+// is a valid JSON string or a null.
+//
+// An empty string will result in a valid-but-empty NullString. The keyword
+// 'null' will result in a null NullString. JSON objects in the form of
+// '{"String":<string>,"Valid":<bool>`}' will decode directly into this
+// NullString. The string '"null"' is considered to be a string -- not a keyword
+// -- and will result in a valid NullString.
+//
+// If the decode fails, the value of this NullString will be unchanged.
+func (s *NullString) UnmarshalJSON(data []byte) error {
+	var j interface{}
+	if err := json.Unmarshal(data, &j); err != nil {
+		return err
+	}
+	switch val := j.(type) {
+	case string:
+		s.String = val
+		s.Valid = true
+		return nil
+	case map[string]interface{}:
+		return json.Unmarshal(data, &s.NullString)
+	case nil:
+		s.String = ""
+		s.Valid = false
+		return nil
+	default:
+		return fmt.Errorf(
+			"null: cannot unmarshal %T (%#v) into Go value of type "+
+				"null.NullString",
+			j, j,
+		)
+	}
+}
+
+// MarshalMapValue implements the pyrrho/encoding/maps Marshaler interface. It
+// will encode this NullString into an interface{} representation for use in a
+// map[string]interface{} if valid, or return nil otherwise.
+func (s NullString) MarshalMapValue() (interface{}, error) {
+	if s.Valid {
+		return s.String, nil
+	}
+	return nil, nil
+}
---
+++ types/null/string_test.go
@@ -0,0 +1,378 @@
+package null_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/pyrrho/encoding/maps"
+	"github.com/pyrrho/encoding/types/null"
+)
+
+// Helper Functions
+
+func assertString(t *testing.T, expected string, b null.NullString, fileLine string) {
+	if !b.Valid {
+		t.Fatalf("%s: NullString is null, but should be valid", fileLine)
+	}
+	if expected != b.String {
+		t.Fatalf("%s: %v ≠ %v", fileLine, expected, b.String)
+	}
+}
+
+func assertNullString(t *testing.T, b null.NullString, fileLine string) {
+	if b.Valid {
+		t.Fatalf("%s: NullString is valid, but should be null", fileLine)
+	}
+}
+
+func TestStringFrom(t *testing.T) {
+	assertString(t, "test", null.StringFrom("test"), FileLine())
+	assertString(t, "", null.StringFrom(""), FileLine())
+}
+
+func TestStringFromPtr(t *testing.T) {
+	s := "test"
+	assertString(t, "test", null.StringFromPtr(&s), FileLine())
+	assertNullString(t, null.StringFromPtr(nil), FileLine())
+}
+
+func TestStringCtor(t *testing.T) {
+	v := "test"
+	var nilPtr *string
+
+	assertString(t, "true", null.String("true"), FileLine())
+	assertString(t, "", null.String(""), FileLine())
+	assertString(t, v, null.String(v), FileLine())
+	assertString(t, v, null.String(&v), FileLine())
+	assertNullString(t, null.String(nil), FileLine())
+	assertNullString(t, null.String(nilPtr), FileLine())
+}
+
+func TestFailureNewStringFromInt(t *testing.T) {
+	defer ShouldPanic(t, FileLine())
+	_ = null.String(0)
+}
+
+func TestFailureNewStringFromBool(t *testing.T) {
+	defer ShouldPanic(t, FileLine())
+	_ = null.String(true)
+}
+
+func TestStringValueOrZero(t *testing.T) {
+	valid := null.String("test")
+	if valid.ValueOrZero() != "test" {
+		t.Fatalf("unexpected ValueOrZero, %v ≠ %v", "test", valid.ValueOrZero())
+	}
+
+	nul := null.NullString{}
+	if nul.ValueOrZero() != "" {
+		t.Fatalf("unexpected ValueOrZero, %v ≠ %v", "", nul.ValueOrZero())
+	}
+}
+
+func TestStringPtr(t *testing.T) {
+	str := null.String("test")
+	ptr := str.Ptr()
+	if *ptr != "test" {
+		t.Fatalf("bad %s string: %#v ≠ %v\n", "pointer", ptr, "test")
+	}
+
+	null := null.NullString{}
+	ptr = null.Ptr()
+	if ptr != nil {
+		t.Fatalf("bad %s string: %#v ≠ %v\n", "nil pointer", ptr, "nil")
+	}
+}
+
+func TestStringSet(t *testing.T) {
+	s := null.NullString{}
+	assertNullString(t, s, FileLine())
+	s.Set("test")
+	assertString(t, "test", s, FileLine())
+	s.Set("")
+	assertString(t, "", s, FileLine())
+}
+
+func TestStringNull(t *testing.T) {
+	s := null.String("test")
+	assertString(t, "test", s, FileLine())
+	s.Null()
+	assertNullString(t, s, FileLine())
+}
+
+func TestStringIsNil(t *testing.T) {
+	a := null.String("test")
+	if a.IsNil() {
+		t.Fatal(`NullString{"test", true}.IsNil() should be false`)
+	}
+	b := null.String("")
+	if b.IsNil() {
+		t.Fatal(`NullString{"", true}.IsNil() should be false`)
+	}
+	nul := null.NullString{}
+	if !nul.IsNil() {
+		t.Fatal("NullString{..., false}.IsNil() should be true")
+	}
+}
+
+func TestStringIsZero(t *testing.T) {
+	a := null.String("test")
+	if a.IsZero() {
+		t.Fatal(`NullString{"test", true}.IsZero() should be false`)
+	}
+	b := null.String("")
+	if !b.IsZero() {
+		t.Fatal(`NullString{"", true}.IsZero() should be true`)
+	}
+	nul := null.NullString{}
+	if !nul.IsZero() {
+		t.Fatal("NullString{..., false}.IsZero() should be true")
+	}
+}
+
+func TestStringSQLValue(t *testing.T) {
+	s := null.String("test")
+	val, err := s.Value()
+	fatalIf(t, err, FileLine())
+	if "test" != val.(string) {
+		t.Fatalf(`NullString{"test", true}.Value() should return a valid driver.Value (string)`)
+	}
+
+	nul := null.NullString{}
+	val, err = nul.Value()
+	fatalIf(t, err, FileLine())
+	if nil != val {
+		t.Fatalf("NullString{..., false}.Value() should return a nil driver.Value")
+	}
+}
+
+func TestStringSQLScan(t *testing.T) {
+	var str null.NullString
+	err := str.Scan("test")
+	fatalIf(t, err, FileLine())
+	assertString(t, "test", str, FileLine())
+
+	var empty null.NullString
+	err = empty.Scan("")
+	fatalIf(t, err, FileLine())
+	assertString(t, "", empty, FileLine())
+
+	var nul null.NullString
+	err = nul.Scan(nil)
+	fatalIf(t, err, FileLine())
+	assertNullString(t, nul, FileLine())
+
+	// NB. Scan is aggressive about converting values to strings. UnmarshalText
+	// and UnmarshalJSON are less so.
+	var i null.NullString
+	err = i.Scan(12345)
+	fatalIf(t, err, FileLine())
+	assertString(t, "12345", i, FileLine())
+
+	var f null.NullString
+	err = f.Scan(1.2345)
+	fatalIf(t, err, FileLine())
+	assertString(t, "1.2345", f, FileLine())
+
+	var b null.NullString
+	err = b.Scan(true)
+	fatalIf(t, err, FileLine())
+	assertString(t, "true", b, FileLine())
+}
+
+func TestStringMarshalText(t *testing.T) {
+	str := null.String("test")
+	data, err := str.MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "test", FileLine())
+	data, err = (&str).MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "test", FileLine())
+
+	zero := null.String("")
+	data, err = zero.MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "", FileLine())
+	data, err = (&zero).MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "", FileLine())
+
+	null := null.NullString{}
+	data, err = null.MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "", FileLine())
+	data, err = (&null).MarshalText()
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "", FileLine())
+}
+
+func TestStringUnmarshalText(t *testing.T) {
+	// Successful Valid Parses
+
+	var str null.NullString
+	err := str.UnmarshalText([]byte("test"))
+	fatalIf(t, err, FileLine())
+	assertString(t, "test", str, FileLine())
+
+	var nullStrQuoted null.NullString
+	err = nullStrQuoted.UnmarshalText([]byte(`"null"`))
+	fatalIf(t, err, FileLine())
+	assertString(t, `"null"`, nullStrQuoted, FileLine())
+
+	var quotes null.NullString
+	err = quotes.UnmarshalText([]byte(`""`))
+	fatalIf(t, err, FileLine())
+	assertString(t, `""`, quotes, FileLine())
+
+	// Successful Null Parses
+
+	var empty null.NullString
+	err = empty.UnmarshalText([]byte(""))
+	fatalIf(t, err, FileLine())
+	assertNullString(t, empty, FileLine())
+
+	var nulString null.NullString
+	err = nulString.UnmarshalText([]byte("null"))
+	fatalIf(t, err, FileLine())
+	assertString(t, "null", nulString, FileLine())
+
+	var nul null.NullString
+	err = nul.UnmarshalText(nil)
+	fatalIf(t, err, FileLine())
+	assertNullString(t, nul, FileLine())
+
+	// Unsuccessful Parses
+	// TODO: Can we... do that with strings? I don't think we can...
+}
+
+func TestStringMarshalJSON(t *testing.T) {
+	str := null.String("test")
+	data, err := json.Marshal(str)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, `"test"`, FileLine())
+	data, err = json.Marshal(&str)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, `"test"`, FileLine())
+
+	zero := null.String("")
+	data, err = json.Marshal(zero)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, `""`, FileLine())
+	data, err = json.Marshal(&zero)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, `""`, FileLine())
+
+	null := null.NullString{}
+	data, err = json.Marshal(null)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "null", FileLine())
+	data, err = json.Marshal(&null)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, data, "null", FileLine())
+}
+
+func TestStringUnmarshalJSON(t *testing.T) {
+	// Successful Valid Parses
+
+	var str null.NullString
+	err := json.Unmarshal(stringJSON, &str)
+	fatalIf(t, err, FileLine())
+	assertString(t, "test", str, FileLine())
+
+	var quotes null.NullString
+	err = json.Unmarshal([]byte(`""`), &quotes)
+	fatalIf(t, err, FileLine())
+	assertString(t, "", quotes, FileLine())
+
+	var nullStr null.NullString
+	err = json.Unmarshal([]byte(`"null"`), &nullStr)
+	fatalIf(t, err, FileLine())
+	assertString(t, "null", nullStr, FileLine())
+
+	var validObj null.NullString
+	err = json.Unmarshal(validStringJSONObj, &validObj)
+	fatalIf(t, err, FileLine())
+	assertString(t, "test", validObj, FileLine())
+
+	var validButEmptyObj null.NullString
+	err = json.Unmarshal(validButEmptyStringJSONObj, &validButEmptyObj)
+	fatalIf(t, err, FileLine())
+	assertString(t, "", validButEmptyObj, FileLine())
+
+	// Successful Null Parses
+
+	var nul null.NullString
+	err = json.Unmarshal([]byte("null"), &nul)
+	fatalIf(t, err, FileLine())
+	assertNullString(t, nul, FileLine())
+
+	var nullObj null.NullString
+	err = json.Unmarshal(nullStringJSONObj, &nullObj)
+	fatalIf(t, err, FileLine())
+	assertNullString(t, nullObj, FileLine())
+
+	// Unsuccessful Parses
+	// TODO: make types for type mismatches on parsing, and check that the
+	// correct error type is being returned here.
+
+	var badType null.NullString
+	// Ints are never string.
+	err = json.Unmarshal(intJSON, &badType)
+	fatalUnless(t, err, FileLine())
+
+	var invalid null.NullString
+	err = invalid.UnmarshalJSON(invalidJSON)
+	if _, ok := err.(*json.SyntaxError); !ok {
+		t.Fatalf("expected json.SyntaxError, not %T", err)
+	}
+}
+
+func TestStringInStructMarshalJSON(t *testing.T) {
+	type stringTestStruct struct {
+		NullString null.NullString `json:"null_string"`
+		String     string          `json:"string"`
+	}
+
+	s := stringTestStruct{
+		NullString: null.String("valid"),
+		String:     "test",
+	}
+	sj, err := json.Marshal(s)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, sj, `{"null_string":"valid","string":"test"}`, FileLine())
+
+	s = stringTestStruct{
+		NullString: null.NullString{},
+		String:     "test",
+	}
+	sj, err = json.Marshal(s)
+	fatalIf(t, err, FileLine())
+	assertJSONEquals(t, sj, `{"null_string":null,"string":"test"}`, FileLine())
+}
+
+func TestStringMarshalMapValue(t *testing.T) {
+	wrapper := struct{ Slice null.NullString }{null.String("test")}
+	data, err := maps.Marshal(wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": "test"}, FileLine())
+	data, err = maps.Marshal(&wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": "test"}, FileLine())
+
+	wrapper = struct{ Slice null.NullString }{null.String("")}
+	data, err = maps.Marshal(wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": ""}, FileLine())
+	data, err = maps.Marshal(&wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": ""}, FileLine())
+
+	// Null NullStrings should be encoded as "nil"
+	wrapper = struct{ Slice null.NullString }{null.NullString{}}
+	data, err = maps.Marshal(wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": nil}, FileLine())
+	data, err = maps.Marshal(&wrapper)
+	fatalIf(t, err, FileLine())
+	assertMapEquals(t, data, map[string]interface{}{"Slice": nil}, FileLine())
+}
