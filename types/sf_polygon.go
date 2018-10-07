77636cb6c11809050a941e06be91219a6088c533
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-10-07 18:12:56 -0400

    Make SFPolygon more consistent, safer, add comments

--- types/sf_polygon.go
+++ types/sf_polygon.go
@@ -10,65 +10,111 @@ import (
 	"github.com/twpayne/go-geom/encoding/wkb"
 )
 
+// SFPolygon is a Simple Feature Polygon, named for the OpenGIS specification
+// that backs WKB, WKT, and GeoJSON representations of geospatial data. An
+// SFPolygon represents a series of [longitude, latitude] or [longitude,
+// latitude, altitude] points in a given coordinate system that make up one
+// external polygon bounding the given shape, and zero or more internal polygons
+// bounding holes within the shape. These polygons should follow the right-hand
+// rule for wrapping order; polygons with a positive area (the external,
+// positive-space shape) should wrap counter-clockwise, and polygons with a
+// negative area (the internal, negative-space holes) should wrap clockwise.
+//
+// This type is built on top of the go-geom geom.Polygon type, implementing all
+// of the pyrrho/encoding/types interfaces detailed in the package comments.
+// Database interactions (Value and Scan) will convert to and from a WKB (Well
+// Known Binary) representation. JSON interactions (MarshalJSON and
+// UnmarshalJSON) will convert to and from a GeoJSON representation.
 type SFPolygon struct {
 	geom.Polygon
 }
 
 // Constructors
 
-func NewSFPolygon(p geom.Polygon) *SFPolygon {
-	return &SFPolygon{p}
+// NewSFPolygon constructs and returns a new SFPolygon object initialized with
+// the given geom.Polygon p.
+func NewSFPolygon(p geom.Polygon) SFPolygon {
+	return SFPolygon{p}
 }
 
-func MustSFPolygon(p *SFPolygon, err error) *SFPolygon {
+// NewSFPolygonXY constructs and returns a new SFPolygon object with longitude
+// and latitude components initialized with the given external and (optionally)
+// internal shapes.
+func NewSFPolygonXY(external [][2]float64, internals ...[][2]float64) SFPolygon {
+	l := len(internals) + 1
+	polys := make([][]geom.Coord, l)
+
+	polys[0] = make([]geom.Coord, len(external))
+	for i := range external {
+		polys[0][i] = append(geom.Coord(nil), external[i][:]...)
+	}
+	for j, internal := range internals {
+		polys[j+1] = make([]geom.Coord, len(internal))
+		for i := range internal {
+			polys[j+1][i] = append(geom.Coord(nil), internal[i][:]...)
+		}
+	}
+
+	p, err := geom.NewPolygon(geom.XY).SetCoords(polys)
 	if err != nil {
 		panic(err)
 	}
-	return p
+	return SFPolygon{*p}
 }
 
-func NewSFPolygonXY(c ...[]geom.Coord) (*SFPolygon, error) {
-	p, err := geom.NewPolygon(geom.XY).SetCoords(c)
-	if err != nil {
-		return nil, err
-	}
-	return &SFPolygon{*p}, err
-}
+// NewSFPolygonXYZ constructs and returns a new SFPolygon object with longitude,
+// latitude, and altitude components initialized with the given external and
+// (optionally) internal shapes.
+func NewSFPolygonXYZ(external [][3]float64, internals ...[][3]float64) SFPolygon {
+	l := len(internals) + 1
+	polys := make([][]geom.Coord, l)
 
-func NewSFPolygonXYM(c ...[]geom.Coord) (*SFPolygon, error) {
-	p, err := geom.NewPolygon(geom.XYM).SetCoords(c)
-	if err != nil {
-		return nil, err
+	polys[0] = make([]geom.Coord, len(external))
+	for i := range external {
+		polys[0][i] = append(geom.Coord(nil), external[i][:]...)
 	}
-	return &SFPolygon{*p}, err
-}
+	for j, internal := range internals {
+		polys[j+1] = make([]geom.Coord, len(internal))
+		for i := range internal {
+			polys[j+1][i] = append(geom.Coord(nil), internal[i][:]...)
+		}
+	}
 
-func NewSFPolygonXYZ(c ...[]geom.Coord) (*SFPolygon, error) {
-	p, err := geom.NewPolygon(geom.XYZ).SetCoords(c)
+	p, err := geom.NewPolygon(geom.XYZ).SetCoords(polys)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	return &SFPolygon{*p}, err
-}
-
-func NewSFPolygonXYZM(c ...[]geom.Coord) (*SFPolygon, error) {
-	p, err := geom.NewPolygon(geom.XYZM).SetCoords(c)
-	if err != nil {
-		return nil, err
-	}
-	return &SFPolygon{*p}, err
+	return SFPolygon{*p}
 }
 
 // Interfaces
 
+// IsNil implements the pyrrho/encoding IsNiler interface. It will return true
+// if p contains no meaningful data. More specifically, if this if this SFPoint
+// has been zero-initialized, or if it has been explicitly initialized with no
+// layout;
+//   var p types.SFPolygon
+//   var p := types.SFPolygon{}
+//   var p := types.NewSFPolygon(geom.Polygon{geom.NoLayout})
+//   var p := types.NewSFPolygonXY(nil)
+//   var p := types.NewSFPolygonXY([][2]float64{})
 func (p SFPolygon) IsNil() bool {
-	return p.FlatCoords() == nil
+	return p.FlatCoords() == nil || p.Layout() == geom.NoLayout
 }
 
+// IsZero implements the pyrrho/encoding IsZeroer interface. It will return true
+// if p.IsNil() returns true, or if the contained data is of the zero-value.
 func (p SFPolygon) IsZero() bool {
-	return len(p.FlatCoords()) == 0
+	for _, f := range p.FlatCoords() {
+		if f != 0.0 {
+			return false
+		}
+	}
+	return true
 }
 
+// Value implements the database/sql/driver Valuer interface. It will return the
+// value of p as a driver.Value; specifically a WKB encoded []byte.
 func (p SFPolygon) Value() (driver.Value, error) {
 	b := &bytes.Buffer{}
 	if err := wkb.Write(b, wkb.NDR, &p.Polygon); err != nil {
@@ -77,6 +123,10 @@ func (p SFPolygon) Value() (driver.Value, error) {
 	return b.Bytes(), nil
 }
 
+// Scan implements the database/sql Scanner interface. It expects to receive a
+// WKB encoded byte describing a Polygon from an SQL database, and will assign
+// that value to p. If the incoming []byte is not a well formed WKB, or if that
+// WKB value does not describe a Polygon, an error will be returned.
 func (p *SFPolygon) Scan(src interface{}) error {
 	if p == nil {
 		return fmt.Errorf("types.SFPolygon: Scan called on nil SFLPolygoner")
@@ -97,6 +147,8 @@ func (p *SFPolygon) Scan(src interface{}) error {
 	return nil
 }
 
+// MarshalJSON implements the encoding/json Marshaler interface. It will return
+// the GeoJSON encoded representation of p.
 func (p SFPolygon) MarshalJSON() ([]byte, error) {
 	if p.IsNil() {
 		return nil, fmt.Errorf("types.SFPolygon: cannot unmarshal an uninitialized SFPolygon")
@@ -104,6 +156,9 @@ func (p SFPolygon) MarshalJSON() ([]byte, error) {
 	return geojson.Marshal(&p.Polygon)
 }
 
+// UnmarshalJSON implements the encoding/json Unmarshaler interface. It expects
+// to receive a valid GeoJSON Geometry with of the type Polygon, and will assign
+// the value of that data to p.
 func (p *SFPolygon) UnmarshalJSON(data []byte) error {
 	if p == nil {
 		return fmt.Errorf("types.SFPolygon: UnmarshalJSON called on nil SFLPolygoner")
@@ -116,6 +171,8 @@ func (p *SFPolygon) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// MarshalMapValue implements the pyrrho/encoding/maps Marshaler interface. It
+// will return p wrapped in an interface{} for use in a map[string]interface{}.
 func (p SFPolygon) MarshalMapValue() (interface{}, error) {
 	return p, nil
 }
--- types/sf_polygon_test.go
+++ types/sf_polygon_test.go
@@ -15,10 +15,10 @@ var (
 	// These are all OpenGIS Simple Feature representations of an XY Polygon with
 	// X == 1.2 and Y == 2.3, converted between representations with
 	// https://rodic.fr/blog/online-conversion-between-geometric-formats/
-	testPolygonWKT     = []byte("POLYGON((30 10,40 40,20 40,10 20,30 10))")
-	testPolygonGeoJSON = []byte(`{"type":"Polygon","coordinates":[[[30,10],[40,40],[20,40],[10,20],[30,10]]]}`)
+	testPolygonWKT     = []byte("POLYGON((30 10,40 40,20 40,10 20,30 10),(28 15,15 21,22 35,35 35,28 15))")
+	testPolygonGeoJSON = []byte(`{"type":"Polygon","coordinates":[[[30,10],[40,40],[20,40],[10,20],[30,10]],[[28,15],[15,21],[22,35],[35,35],[28,15]]]}`)
 	testPolygonWKB     = []byte{
-		0x01, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00,
+		0x01, 0x03, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
 		0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x3e, 0x40, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x24, 0x40, 0x00, 0x00, 0x00,
@@ -29,15 +29,49 @@ var (
 		0x00, 0x00, 0x00, 0x24, 0x40, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x34, 0x40, 0x00, 0x00, 0x00,
 		0x00, 0x00, 0x00, 0x3e, 0x40, 0x00, 0x00, 0x00,
-		0x00, 0x00, 0x00, 0x24, 0x40,
+		0x00, 0x00, 0x00, 0x24, 0x40, 0x05, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3c,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2e,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2e,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x35,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x36,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x41,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x41,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x41,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3c,
+		0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2e,
+		0x40,
 	}
-	testPolygonCoords = [][]geom.Coord{{
+	testPolygonExternal = [][2]float64{
 		{30, 10},
 		{40, 40},
 		{20, 40},
 		{10, 20},
 		{30, 10},
-	}}
+	}
+	testPolygonInternal = [][2]float64{
+		{28, 15},
+		{15, 21},
+		{22, 35},
+		{35, 35},
+		{28, 15},
+	}
+	testPolygonCoords = [][]geom.Coord{
+		{
+			{30, 10},
+			{40, 40},
+			{20, 40},
+			{10, 20},
+			{30, 10},
+		},
+		{
+			{28, 15},
+			{15, 21},
+			{22, 35},
+			{35, 35},
+			{28, 15},
+		},
+	}
 	testPolygonGoGeom = *geom.NewPolygon(geom.XY).MustSetCoords(testPolygonCoords)
 )
 
@@ -47,52 +81,66 @@ func TestSFPolygonCtors(t *testing.T) {
 	// types.SFPolygon is a wrapper around go-geom's Polygon class. As such,
 	// construction typically uses their conventions.
 	pa := types.NewSFPolygon(*geom.NewPolygon(geom.XY).MustSetCoords(
-		[][]geom.Coord{{
+		[][]geom.Coord{
+			{
+				{30, 10},
+				{40, 40},
+				{20, 40},
+				{10, 20},
+				{30, 10},
+			},
+			{
+				{28, 15},
+				{15, 21},
+				{22, 35},
+				{35, 35},
+				{28, 15},
+			},
+		}))
+	require.Equal(testPolygonCoords, pa.Coords())
+	require.Equal(testPolygonGoGeom, pa.Polygon)
+	// We have some helpers to make it easier, though.
+	pb := types.NewSFPolygonXY(
+		[][2]float64{
 			{30, 10},
 			{40, 40},
 			{20, 40},
 			{10, 20},
 			{30, 10},
-		}}))
-	require.Equal(testPolygonCoords, pa.Coords())
-	require.Equal(testPolygonGoGeom, pa.Polygon)
-
-	// We have some helpers to make it easier, though.
-	pb, err := types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	})
-	require.NoError(err)
-	pc, err := types.NewSFPolygonXYZ([]geom.Coord{
+		},
+		[][2]float64{
+			{28, 15},
+			{15, 21},
+			{22, 35},
+			{35, 35},
+			{28, 15},
+		})
+	require.Equal(
+		*geom.NewPolygon(geom.XY).MustSetCoords(
+			[][]geom.Coord{
+				{
+					{30, 10},
+					{40, 40},
+					{20, 40},
+					{10, 20},
+					{30, 10},
+				},
+				{
+					{28, 15},
+					{15, 21},
+					{22, 35},
+					{35, 35},
+					{28, 15},
+				},
+			}),
+		pb.Polygon)
+	pc := types.NewSFPolygonXYZ([][3]float64{
 		{30, 10, 1},
 		{40, 40, 2},
 		{20, 40, 3},
 		{10, 20, 4},
 		{30, 10, 5},
 	})
-	require.NoError(err)
-	pd, err := types.NewSFPolygonXYZM([]geom.Coord{
-		{30, 10, 1, 42},
-		{40, 40, 2, 42},
-		{20, 40, 3, 42},
-		{10, 20, 4, 42},
-		{30, 10, 5, 42},
-	})
-	require.NoError(err)
-
-	require.Equal(
-		*geom.NewPolygon(geom.XY).MustSetCoords(
-			[][]geom.Coord{{
-				{30, 10},
-				{40, 40},
-				{20, 40},
-				{10, 20},
-				{30, 10}},
-			}),
-		pb.Polygon)
 	require.Equal(
 		*geom.NewPolygon(geom.XYZ).MustSetCoords(
 			[][]geom.Coord{{
@@ -103,43 +151,27 @@ func TestSFPolygonCtors(t *testing.T) {
 				{30, 10, 5},
 			}}),
 		pc.Polygon)
-	require.Equal(
-		*geom.NewPolygon(geom.XYZM).MustSetCoords(
-			[][]geom.Coord{{
-				{30, 10, 1, 42},
-				{40, 40, 2, 42},
-				{20, 40, 3, 42},
-				{10, 20, 4, 42},
-				{30, 10, 5, 42},
-			}}),
-		pd.Polygon)
-
-	// If you do something bad with one of these constructors, you'll get the
-	// error expected from go-geom.
-	_, err = types.NewSFPolygonXY([]geom.Coord{
-		{30, 10, 1, 42},
-		{40, 40, 2, 42},
-		{20, 40, 3, 42},
-		{10, 20, 4, 42},
-		{30, 10, 5, 42},
-	})
-	require.Error(err) // geom: stride mismatch, got 4, want 2
 }
 
 func TestSFPolygonIsNil(t *testing.T) {
 	require := require.New(t)
 
-	p, _ := types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10}})
+	p := types.NewSFPolygonXY(testPolygonExternal, testPolygonInternal)
 	require.False(p.IsNil())
 
-	// NB. I consider this a defect, but it's how go-geom is implemented, so...
-	zero, _ := types.NewSFPolygonXY([]geom.Coord{})
+	malformed := types.NewSFPolygonXY([][2]float64{
+		{0.0, 0.0},
+		{0.0, 0.0},
+		{0.0, 0.0},
+		{0.0, 0.0},
+	})
+	require.False(malformed.IsNil())
+
+	zero := types.NewSFPolygonXY([][2]float64{})
 	require.True(zero.IsNil())
+
+	nul := types.NewSFPolygonXY(nil)
+	require.True(nul.IsNil())
 
 	empty := types.SFPolygon{}
 	require.True(empty.IsNil())
@@ -148,16 +180,22 @@ func TestSFPolygonIsNil(t *testing.T) {
 func TestSFPolygonIsZero(t *testing.T) {
 	require := require.New(t)
 
-	p, _ := types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10}})
+	p := types.NewSFPolygonXY(testPolygonExternal, testPolygonInternal)
 	require.False(p.IsZero())
 
-	zero, _ := types.NewSFPolygonXY([]geom.Coord{})
+	malformed := types.NewSFPolygonXY([][2]float64{
+		{0.0, 0.0},
+		{0.0, 0.0},
+		{0.0, 0.0},
+		{0.0, 0.0},
+	})
+	require.True(malformed.IsZero())
+
+	zero := types.NewSFPolygonXY([][2]float64{})
 	require.True(zero.IsZero())
+
+	nul := types.NewSFPolygonXY(nil)
+	require.True(nul.IsZero())
 
 	empty := types.SFPolygon{}
 	require.True(empty.IsZero())
@@ -168,14 +206,7 @@ func TestSFPolygonSQLValue(t *testing.T) {
 	var val driver.Value
 	var err error
 
-	p, err := types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	})
-	require.NoError(err)
+	p := types.NewSFPolygonXY(testPolygonExternal, testPolygonInternal)
 	val, err = p.Value()
 	require.NoError(err)
 	require.EqualValues(testPolygonWKB, val)
@@ -188,14 +219,7 @@ func TestSFPolygonSQLScan(t *testing.T) {
 	var p types.SFPolygon
 	err = p.Scan(driver.Value(testPolygonWKB))
 	require.NoError(err)
-	require.Equal(
-		[][]geom.Coord{{
-			{30, 10},
-			{40, 40},
-			{20, 40},
-			{10, 20},
-			{30, 10},
-		}}, p.Coords())
+	require.Equal(testPolygonCoords, p.Coords())
 
 	var bad types.SFPolygon
 	err = bad.Scan(driver.Value(nil))
@@ -207,13 +231,7 @@ func TestSFPolygonMarshalJSON(t *testing.T) {
 	var data []byte
 	var err error
 
-	p, _ := types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	})
+	p := types.NewSFPolygonXY(testPolygonExternal, testPolygonInternal)
 	data, err = json.Marshal(p)
 	require.NoError(err)
 	require.EqualValues(testPolygonGeoJSON, data)
@@ -235,13 +253,7 @@ func TestSFPolygonUnmarshalJSON(t *testing.T) {
 	var p types.SFPolygon
 	err = json.Unmarshal(testPolygonGeoJSON, &p)
 	require.NoError(err)
-	require.Equal([][]geom.Coord{{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	}}, p.Coords())
+	require.Equal(testPolygonCoords, p.Coords())
 }
 
 func TestSFPolygonMarshsalMapValue(t *testing.T) {
@@ -251,29 +263,11 @@ func TestSFPolygonMarshsalMapValue(t *testing.T) {
 	var data map[string]interface{}
 	var err error
 
-	wrapper = Wrapper{*types.MustSFPolygon(types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	}))}
+	wrapper = Wrapper{types.NewSFPolygonXY(testPolygonExternal, testPolygonInternal)}
 	data, err = maps.Marshal(wrapper)
 	require.NoError(err)
-	require.Equal(*types.MustSFPolygon(types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	})), data["Polygon"])
+	require.Equal(types.NewSFPolygon(testPolygonGoGeom), data["Polygon"])
 	data, err = maps.Marshal(&wrapper)
 	require.NoError(err)
-	require.Equal(*types.MustSFPolygon(types.NewSFPolygonXY([]geom.Coord{
-		{30, 10},
-		{40, 40},
-		{20, 40},
-		{10, 20},
-		{30, 10},
-	})), data["Polygon"])
+	require.Equal(types.NewSFPolygon(testPolygonGoGeom), data["Polygon"])
 }
