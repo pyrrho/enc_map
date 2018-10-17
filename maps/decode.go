ac9253b1c07571f5eaaf19fd87c40f7e0fdf3e15
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2018-10-16 22:33:30 -0400

    Sketch out the interface for encoding/maps/Unmarshal

--- maps/decode.go
+++ maps/decode.go
@@ -1,1 +1,38 @@
 package maps
+
+import (
+	"errors"
+	"reflect"
+)
+
+func Unmarshal(src interface{}, v interface{}) error {
+	err := defaultConfig.unmarshal(src, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type Unmarshaler interface {
+	UnmarshalMapValue() error
+}
+
+var unmarshalerType = reflect.TypeOf(new(Unmarshaler)).Elem()
+
+func (cfg *Config) Unmarshal(src interface{}, v interface{}) error {
+	err := cfg.unmarshal(src, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cfg *Config) unmarshal(src interface{}, v interface{}) error {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr {
+		return errors.New("encoding/maps: cannot unmarshal into non-pointer")
+	} else if rv.IsNil() {
+		return errors.New("encoding/maps: cannot unmarshal into nil pointer")
+	}
+	return errors.New("enc_map.Scan has not yet been implemented")
+}
